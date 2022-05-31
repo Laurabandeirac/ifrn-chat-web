37dd56d83ae7d1b9042c3c627e67c7a1bbbea6c5
Laurabandeirac <laura.bamdeira@escolar.ifrn.edu.br>
2022-05-31 15:52:26 -0300

    atividade do dia trinta e um de maio

--- pages/login.js
+++ pages/login.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { AppContainer } from '../src/components/AppContainer';
 import { AppInput } from '../src/components/AppInput';
 import { AppButton } from '../src/components/AppButton';
-import { AppContainer } from '../src/components/AppContainer';
 
 const Login = props => {
+  const router = useRouter();
+
   return (
-		<AppContainer>
-			<AppInput
-				title="apelido ou email do usuário"
-				label="Usuário:"
-				type="text"
-			/>
-			<AppInput 
-				label="Senha:" 
-				type="password" 
-				title="senha do usuário" 
-			/>
-			<AppButton title="Entrar" />
-		</AppContainer>
-	);
+    <div>
+      <Head>
+        <title>página de login</title>
+      </Head>
+      <main>
+        <nav>
+          <p>Chat de Infoweb</p>
+          <button onClick={() => router.back()}>voltar</button>
+        </nav>
+        <AppContainer>
+          <AppInput label="Usuário" title="apelido ou email do usuário"/>
+          <AppInput label="Senha" type="password" />
+          <AppButton title="Clique aqui para entrar no chat" label="Entrar no chat" />
+        </AppContainer>
+      </main>
+      </div>
+  );
 };
 
 export default Login;
